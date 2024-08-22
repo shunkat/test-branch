a087bb9a86cfa9f0a50722f6048e1b41b5f2bb1d
shunkat <shunkat@icloud.com>
2024-08-22 14:28:45 +0900

    add seeds data

---
+++ supabase/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO notes (id, title, new_column)
+VALUES
+    (1, 'Today I created a Supabase project.', 'new'),
+    (2, 'I added some data and queried it from Next.js.', 'data'),
+    (3, 'It was awesome!', 'is added');
