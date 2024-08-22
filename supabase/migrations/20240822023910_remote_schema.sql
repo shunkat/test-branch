8b9da6bed6ae4ed2f92706d87e474f7a842d2fb0
shunkat <shunkat@icloud.com>
2024-08-22 11:44:20 +0900

    Initial migration

---
+++ supabase/migrations/20240822023910_remote_schema.sql
@@ -0,0 +1,63 @@
+create table "public"."notes" (
+    "id" bigint generated always as identity not null,
+    "title" text not null
+);
+
+
+alter table "public"."notes" enable row level security;
+
+CREATE UNIQUE INDEX notes_pkey ON public.notes USING btree (id);
+
+alter table "public"."notes" add constraint "notes_pkey" PRIMARY KEY using index "notes_pkey";
+
+grant delete on table "public"."notes" to "anon";
+
+grant insert on table "public"."notes" to "anon";
+
+grant references on table "public"."notes" to "anon";
+
+grant select on table "public"."notes" to "anon";
+
+grant trigger on table "public"."notes" to "anon";
+
+grant truncate on table "public"."notes" to "anon";
+
+grant update on table "public"."notes" to "anon";
+
+grant delete on table "public"."notes" to "authenticated";
+
+grant insert on table "public"."notes" to "authenticated";
+
+grant references on table "public"."notes" to "authenticated";
+
+grant select on table "public"."notes" to "authenticated";
+
+grant trigger on table "public"."notes" to "authenticated";
+
+grant truncate on table "public"."notes" to "authenticated";
+
+grant update on table "public"."notes" to "authenticated";
+
+grant delete on table "public"."notes" to "service_role";
+
+grant insert on table "public"."notes" to "service_role";
+
+grant references on table "public"."notes" to "service_role";
+
+grant select on table "public"."notes" to "service_role";
+
+grant trigger on table "public"."notes" to "service_role";
+
+grant truncate on table "public"."notes" to "service_role";
+
+grant update on table "public"."notes" to "service_role";
+
+create policy "public can read notes"
+on "public"."notes"
+as permissive
+for select
+to anon
+using (true);
+
+
+
